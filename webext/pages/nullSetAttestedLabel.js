10746a387f5e707a9081ae4518b5ff3269f9de33
Matthew Finkel <sysrqb@torproject.org>
2020-06-04 03:09:28 +0000

    Fill in the values on the error page

--- webext/pages/nullSetAttestedLabel.js
+++ webext/pages/nullSetAttestedLabel.js
@@ -5,17 +5,17 @@ function setDomain(get) {
 
 function setLabelSet(get) {
     let e = document.getElementById("selfLabels");
-    e.appendChild(document.createTextNode(get.labels));
+    e.appendChild(document.createTextNode(get.selfLabels));
 }
 
 function setSatLabelSet(get) {
     let e = document.getElementById("satLabels");
-    e.appendChild(document.createTextNode(get.labels));
+    e.appendChild(document.createTextNode(get.satLabels));
 }
 
 function setErrorMessage(get) {
     let e = document.getElementById("errmsg");
-    e.appendChild(document.createTextNode(get.error));
+    e.appendChild(document.createTextNode(decodeURIComponent(get.error)));
 }
 
 function addButtonTargets() {
