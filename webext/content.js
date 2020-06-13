7c3423b4a2aed21aff7dbf67aa80b5aacc4ed0b6
Matthew Finkel <sysrqb@torproject.org>
2020-06-13 01:56:07 +0000

    Comment out in-page sat domain lists search

--- webext/content.js
+++ webext/content.js
@@ -19,14 +19,14 @@ function main() {
                 "url": document.URL, "satSig": satSig });
         response.then(redirect_sat_page, log_error_from_bg);
     }
-    let set = findSATDomainList(document);
-    if (!set || set.size < 1) {
-        return;
-    }
-    let response = sendMessage(
-        "satDomainList", {
-            "url": document.URL, "set": set, "wellknown": false, "satUrl": false });
-    response.then(log_success_from_bg, log_error_from_bg);
+    //let set = findSATDomainList(document);
+    //if (!set || set.size < 1) {
+    //    return;
+    //}
+    //let response = sendMessage(
+    //    "satDomainList", {
+    //        "url": document.URL, "set": set, "wellknown": false, "satUrl": false });
+    //response.then(log_success_from_bg, log_error_from_bg);
     fetchSATDomainList(window);
 }
 
